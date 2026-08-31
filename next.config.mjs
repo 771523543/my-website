8c5fb7d86ee575c7da6fe90167b9bcc723d2c12c
771523543 <adelbasheer31@gmail.com>
2026-08-31 23:54:59 +0300

    Updated next-env.d.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
+    ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
