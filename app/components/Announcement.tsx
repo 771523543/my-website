4ea0d28d37c27d0e85064f69a925f83adef1fbcc
771523543 <adelbasheer31@gmail.com>
2026-09-07 05:54:27 +0300

    Delete app/components directory

--- app/components/Announcement.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ArrowLeft, Sparkles } from 'lucide-react'
-
-export default function Announcement() {
-  return (
-<div className="announcement"><Sparkles size={15} /> خصم خاص على خدمات منصة هديل لفترة محدودة <ArrowLeft size={15} /></div>
-  )
-}
